b3d4edd296d3e0640f2f4e34828cf3b368c12f9b
Huang_Kuan_Lun <jian5753@gmail.com>
2020-10-21 00:17:06 +0800

    sim container

--- main.py
+++
@@ -1,4 +0,0 @@
-import simContainer as SC
-
-test = SC.simContainer([10, 1.5], [0, 0], [1, 1], [[1, 0], [0, 1]], 10, 10 ,10)
-print(test.simResult.shape)
--- simContainer.py
+++ simContainer.py
@@ -1,17 +1,49 @@
 import numpy as np
+from scipy.linalg import cholesky
 
-class simContainer():
-    def __init__(self, s0Arr, muArr, sigmaArr, corrMatrx, timePeriod, simPathCnt, trialCnt):
-        self.s0Arr = np.array(s0Arr) 
-        self.muArr = np.array(muArr)
-        self.sigmaArr = np.array(sigmaArr)
-        self.corrMatrx = np.array(corrMatrx)
+class multiVariateSim():
+    def __init__(self, S0Arr, riskFreeRate, timePeriod, timePartitionCnt, qArr, sigmaArr, corrMatrx, simCnt, repeatCnt):
+        self.lnS0Arr = np.log(np.array(S0Arr))
+        self.assetCnt = self.lnS0Arr.shape[0]
+        self.riskFreeRate = riskFreeRate
         self.timePeriod = timePeriod
-        self.simPathCnt = simPathCnt
-        self.trialCnt = trialCnt
+        self.timePartitionCnt = timePartitionCnt
+        self.qArr = np.array(qArr)
+        self.sigmaArr = np.array(sigmaArr)
+        self.corrMtrx = np.array(corrMatrx)
+        self.simCnt = simCnt
+        self.repeatCnt = repeatCnt
+        self.covMtrx = self.corrMtrx * np.tensordot(self.sigmaArr.reshape(1, -1), self.sigmaArr.reshape(1, -1), axes=(0,0)) 
 
-        self.assetCnt = self.muArr.shape[0]
-        self.simResult = np.zeros((self.assetCnt, timePeriod, simPathCnt * trialCnt))
+        self.Z0 = np.random.normal(
+            loc= 0.0,
+            scale= 1.0,
+            size= (self.repeatCnt, self.simCnt, self.timePartitionCnt - 1, self.assetCnt)
+        )
 
-    def sim(self):
-        raise KeyboardInterrupt
+        self.returnSeries = np.zeros_like(self.Z0)
+
+        self.mu_delta_t_return = (self.riskFreeRate - self.qArr - np.power(self.sigmaArr, 2) / 2) * (self.timePeriod / self.timePartitionCnt)
+
+
+    def var_cov(self):
+        utri = cholesky(self.covMtrx)
+        # var-cov
+        self.returnSeries = np.tensordot(self.Z0, utri, axes = ([3], [0]))
+
+    def muShift(self):
+        # mu shift
+        self.returnSeries += self.mu_delta_t_return
+
+    def lnS0Append(self):
+        # append lnS0
+        self.lnS0ArrPanel = self.lnS0Arr * np.ones((self.repeatCnt, self.simCnt, 1, self.assetCnt))
+        self.lnStSeries = np.concatenate((self.lnS0ArrPanel, self.returnSeries), axis= 2)
+    
+    def cumsum(self):
+        # cumsum
+        self.lnStSeries = self.lnStSeries.cumsum(axis= 2)
+        self.stSeries = np.exp(self.lnStSeries)
+
+
+
